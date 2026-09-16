f1e1c9f70126713ce03d5c86c3b12f02cd409725
MahadSardar <mahadsardarm2@gmail.com>
2026-09-16 19:17:19 +0500

    Initial comment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
-import rateLimiter from "./middleware/ratelimiter.js";
+import rateLimiter from "./middleware/rateLimiter.js";
 import authRoutes from "./routes/authRoutes.js"
 import cors from "cors";
 
